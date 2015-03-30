eecc86a87b58376a809451172acba27ab184d975
neil patel <neil@myindianweddingapp.com>
2015-03-30 11:00:54 +0100

    Install and set up devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Devise. Remeber to change localhost:300 to actual application host
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
